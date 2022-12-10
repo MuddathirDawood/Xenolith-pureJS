ee43caff0d76fd439c5e138d2545911bdff942b9
MuddathirDawood <dmuddathir@gmail.com>
2022-12-10 11:20:17 +0200

    xenolith-pureJS

---
+++ script/admin.js
@@ -0,0 +1,106 @@
+let games = JSON.parse(localStorage.getItem('games'))
+
+let addTag = document.querySelector('.table')
+function displayData(){
+    games.forEach((item, index) => {
+        addTag.innerHTML += `
+        <tr class="table__row">
+        <td class="table__content" data-heading="ID">${item.id}</td>
+        <td class="table__content" data-heading="Title">${item.title}</td>
+        <td class="table__content" data-heading="Price">R${item.price}</td>
+        <td class="table__content" data-heading="Platform">${item.platform}</td>
+        <td class="table__content" data-heading="Cover"><img id="coverImg" src="${item.cover}" alt="Cover"></td>
+        <td class="table__content" data-heading="Options">
+        <button onclick="editGame(${index})"  data-bs-toggle="modal" data-bs-target="#editModal">Edit</button>
+        <button onclick="deleteGame(${index})">Delete</button>
+        </td>
+        </tr>
+        `
+    });
+}
+
+displayData()
+
+
+function addItem(){
+    let id = games.length + 1
+    let title = document.querySelector('.title').value
+    let platform = document.querySelector('.platform').value
+    let price = document.querySelector('.price').value
+    let cover = document.querySelector('.cover').value
+
+    games.push({
+        id,
+        title,
+        platform,
+        price,
+        cover
+    })
+    console.log(games);
+    localStorage.setItem('games', JSON.stringify(games))
+    addTag.innerHTML = ''
+    displayData()
+}
+
+
+let gameID
+
+function editGame(index){
+    let game = games[index]
+    gameID = game.id
+    document.querySelector('.etitle').value = game.title
+    document.querySelector('.eplatform').value = game.platform
+    document.querySelector('.eprice').value = game.price
+    document.querySelector('.ecover').value = game.cover
+}
+
+function edit(){
+    console.log(gameID);
+    let id = gameID
+    let title = document.querySelector('.etitle').value
+    let platform = document.querySelector('.eplatform').value
+    let price = document.querySelector('.eprice').value
+    let cover = document.querySelector('.ecover').value
+    let game = games[gameID-1]
+    game = {
+        id,
+        title,
+        platform,
+        price,
+        cover
+    }
+    games[gameID-1] = game
+    localStorage.setItem('games', JSON.stringify(games))
+    addTag.innerHTML = `
+    <tr>
+       <th class="table__heading">ID</th>
+       <th class="table__heading">Title</th>
+       <th class="table__heading">Price</th>
+       <th class="table__heading">Platform</th>
+       <th class="table__heading">Cover</th>
+       <th class="table__heading">Options</th>
+     </tr>
+    `
+    displayData()
+}
+
+function deleteGame(index){
+    games.splice(index, 1)
+    let i = 1
+    games.forEach(item => {
+        item.id = i
+        i++
+    })
+    localStorage.setItem('games', JSON.stringify(games))
+    addTag.innerHTML = `
+    <tr>
+       <th class="table__heading">ID</th>
+       <th class="table__heading">Title</th>
+       <th class="table__heading">Price</th>
+       <th class="table__heading">Platform</th>
+       <th class="table__heading">Cover</th>
+       <th class="table__heading">Options</th>
+     </tr>
+    `    
+    displayData()
+}
---
+++ script/index.js
@@ -0,0 +1,58 @@
+let games = JSON.parse(localStorage.getItem('games')) ?
+JSON.parse(localStorage.getItem('games')): 
+[
+    {
+        id: 1,
+        title: 'Legend Of Zelda: BoTW',
+        platform: 'Nintendo Switch',
+        price: 1159,
+        cover: '../assets/products/The Legend of Zelda BoTW-Nintendo Switch.jpg'
+    },
+    {
+        id: 2,
+        title: 'Dying light 2 Stay Human',
+        platform: 'PC',
+        price: 852,
+        cover: '../assets/products/DyingLight2StayHuman.jpg'
+    },    {
+        id: 3,
+        title: 'Crysis 1',
+        platform: 'PC',
+        price: 726,
+        cover: '../assets/products/Crysis1.jpg'
+    }
+]
+localStorage.setItem('games',JSON.stringify(games))
+
+let section = document.querySelector('.prods')
+function displayData(array){
+    array.forEach((item)=> {
+        section.innerHTML += `
+        <div class="card">
+            <img src="${item.cover}" class="card-img-top" alt="Cover">
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">R${item.price}</p>
+              <a href="#" class="btn btn-primary">Add To Checkout</a>
+            </div>
+        </div>
+        `
+    })
+}
+displayData(games)
+
+
+let filter = document.querySelector('#platforms')
+
+filter.addEventListener('change', (e) => {
+    if (e.target.value === 'all') {
+        section.innerHTML = ''
+        displayData(games)
+    } else {
+        let filteredArr = games.filter(item => {
+            return item.platform === e.target.value
+        })
+        section.innerHTML = ''
+        displayData(filteredArr)
+    }
+})
